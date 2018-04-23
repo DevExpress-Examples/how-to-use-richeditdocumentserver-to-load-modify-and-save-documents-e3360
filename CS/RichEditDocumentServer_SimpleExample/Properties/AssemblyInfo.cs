7600ea471a927a3a1f530ece808d5a4050e23365
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RichEditDocumentServer_SimpleExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RichEditDocumentServer_SimpleExample")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RichEditDocumentServer_SimpleExample")]
+[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("934c7fd9-6485-440f-9864-d07a3c0e3739")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VB/RichEditDocumentServer_SimpleExample/MainPage.xaml.vb
@@ -0,0 +1,96 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+Imports System.IO
+Imports DevExpress.XtraRichEdit
+Imports DevExpress.XtraRichEdit.API.Native
+Imports System.Windows.Media.Imaging
+Imports DevExpress.Office.Utils
+
+Namespace RichEditDocumentServer_SimpleExample
+	Partial Public Class MainPage
+		Inherits UserControl
+		Private richServer As RichEditDocumentServer
+		Private imageIndex As Integer = 0
+		Private imgs As DocumentImageCollection
+
+		Public Sub New()
+			InitializeComponent()
+
+		End Sub
+
+Private Sub button1_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+	Dim ofdlg As New OpenFileDialog()
+	ofdlg.Multiselect = False
+	ofdlg.Filter = "Word 97-2003 Files (*.doc)|*.doc"
+	If ofdlg.ShowDialog() = True Then
+		image1.Source = Nothing
+		textBlock1.Text = String.Empty
+'		#Region "#richserverload"
+		richServer = New RichEditDocumentServer()
+		richServer.CreateNewDocument()
+		Try
+			richServer.LoadDocument(ofdlg.File.OpenRead(), DocumentFormat.Doc)
+			imgs = richServer.Document.GetImages(richServer.Document.Range)
+			If imgs.Count > 0 Then
+				ShowCurrentImage()
+			End If
+			textBlock1.Text = richServer.Document.Text
+		Catch ex As Exception
+			textBlock1.Text = "Exception occurs:" & Constants.vbLf + ex.Message
+		End Try
+'		#End Region ' #richserverload
+
+		button2.IsEnabled = True
+		AddHandler SimpleAnimation.Completed, AddressOf SimpleAnimaton_Completed
+		Me.SimpleAnimation.Begin()
+	End If
+End Sub
+
+Private Sub SimpleAnimaton_Completed(ByVal sender As Object, ByVal e As EventArgs)
+	If imageIndex = imgs.Count-1 Then
+	imageIndex = 0
+	Else
+	imageIndex += 1
+	End If
+
+	ShowCurrentImage()
+
+	Me.SimpleAnimation.Begin()
+End Sub
+
+		Private Sub button2_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+'			#Region "#richserversave"
+			Dim doc As SubDocument = richServer.Document.Sections(0).BeginUpdateFooter()
+			Dim docRange As DocumentRange = doc.InsertText(doc.Range.Start, textBox1.Text)
+			doc.InsertParagraph(docRange.End)
+			richServer.Document.Sections(0).EndUpdateFooter(doc)
+
+			Dim sfdlg As New SaveFileDialog()
+			sfdlg.DefaultExt = ".docx"
+			sfdlg.Filter = "Word Document (*.docx)|*.docx"
+			If sfdlg.ShowDialog() = True Then
+				Dim fs As Stream = sfdlg.OpenFile()
+				richServer.SaveDocument(fs, DocumentFormat.OpenXml)
+				fs.Close()
+			End If
+'			#End Region ' #richserversave
+		End Sub
+		Private Sub ShowCurrentImage()
+			Dim ms As New MemoryStream(imgs(imageIndex).Image.GetImageBytesSafe(OfficeImageFormat.Png))
+			Dim bi As New BitmapImage()
+			bi.SetSource(ms)
+			image1.Source = bi
+			ms.Close()
+		End Sub
+	End Class
+End Namespace
